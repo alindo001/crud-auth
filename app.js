e0a3ccd028903bec8bc231b95cf652f858b1a728
alindo <andreslindo20@gmail.com>
2022-08-09 18:05:46 -0400

    dashboard update

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
+const { default: mongoose } = require("mongoose")
 
 
 
@@ -36,6 +38,9 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
     
 }))
 
